dd7313f71d822d7f58b30972f3ba076d9a572691
laboratorio <laboratorio@LAB01DT38>
2025-05-09 11:46:57 -0300

    Adicionar arquivos de projeto.

---
+++ UFNCC-DotNetASPTeste/Pages/Index.cshtml.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace UFNCC_DotNetASPTeste.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            //E chamado toda vez que essa pagina é carregada.
+            Console.WriteLine("Index foi acessada");
+        }
+    }
+}
